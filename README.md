b3a6ce763665e14c36648e465578e7a0aa72a239
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-05-05 08:08:26 +0530

    May 5 2022

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # program-scripts
 
 All the Program scripts in this Github Repository.
-Github Token : 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
-Origin : git remote add origin https://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@github.com/zeoxd/program-scripts.git/
+Github Token : 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
+Origin : git remote add origin https://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@github.com/zeoxd/program-scripts.git/
--- lc/888_fair_candy_ddash.cpp
+++ lc/888_fair_candy_ddash.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
 #include<vector>
+#include<unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
-        
+        int sumA = 0;
+        int sumB = 0;
+
+        for(auto a: aliceSizes) {
+            sumA += a;
+        }
+
+        for(auto b: bobSizes) {
+            sumB += b;
+        }
+
+        int diff = (sumA - sumB) / 2;
+
+        unordered_set<int> us(aliceSizes.begin(), aliceSizes.end());
+
+        for(auto b: bobSizes) {
+            if(us.count(b + diff)) {
+                return {b, b+diff};
+            }
+        }
+
+        return {};
     }
 };
