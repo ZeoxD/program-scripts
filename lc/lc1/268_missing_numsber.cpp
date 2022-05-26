5367797eb6ca2198c8ef7e2eb6d4047645b97254
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-05-26 10:42:49 +0530

    May 26 2022

--- lc/lc1/268_missing_numsber.cpp
+++ lc/lc1/268_missing_numsber.cpp
@@ -6,6 +6,20 @@ using namespace std;
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        
+        int size = nums.size();
+
+        vector<int> ht(size+1, 0);
+
+        for(auto n: nums) {
+            ht[n]++;
+        }
+
+        for(int i = 0; i < size+1; i++) {
+            if(ht[i] == 0) {
+                return i;
+            }
+        }
+
+        return 0;
     }
 };
