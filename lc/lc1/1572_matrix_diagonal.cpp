afc483e6a178c170e08b7c2fb4b67411ccd09a1b
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-06-04 02:18:14 +0530

    Jun 4 2022

--- lc/lc1/1572_matrix_diagonal.cpp
+++ lc/lc1/1572_matrix_diagonal.cpp
@@ -6,12 +6,18 @@ class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
         int rows = mat.size();
-        int cols = mat[0].size();
 
+        int maj_sum = 0, min_sum = 0;
         for(int i = 0; i < rows; i++) {
-            for(int j = 0; j < cols; j++) {
-                
-            }
+            maj_sum += mat[i][i];
+            min_sum += mat[i][(rows-1)-i];
+
         }
+
+        if(rows%2!=0) {
+            return (maj_sum + min_sum) - mat[rows/2][rows/2];
+        }
+
+        return (maj_sum + min_sum);
     }
 };
