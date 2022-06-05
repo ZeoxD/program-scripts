a3a56cb4f69fe000b9a560c3780881e7945adbd9
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-06-05 20:28:58 +0530

    Jun 5 2022

--- lc/lc2/821_shortest_distance.cpp
+++ lc/lc2/821_shortest_distance.cpp
@@ -7,6 +7,31 @@ using namespace std;
 class Solution {
 public:
     vector<int> shortestToChar(string s, char c) {
-        
+        int len = s.length();
+        vector<int> vtr(len, INT8_MAX);
+        int count = 0;
+        bool flag = false;
+        for(int i = 0; i < len; i++) {
+            if(c == s[i]) {
+                count = 0;
+                flag = true;
+            }
+            if(flag)
+            {vtr[i] = count;}
+            ++count;
+        } 
+        flag = false;
+        for(int i = len-1; i >= 0; i--) {
+            if(c == s[i]) {
+                count = 0;
+                flag = true;
+            }
+            if(count < vtr[i] && flag) {
+                vtr[i] = count;
+            }
+            ++count;
+        } 
+
+        return vtr;
     }
 };
