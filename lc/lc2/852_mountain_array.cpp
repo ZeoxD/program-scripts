a194d0bcd3e3c8d8ac9d376deb4913209848d37f
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-06-11 03:59:14 +0530

    Jun 11 2022

---
+++ lc/lc2/852_mountain_array.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int size = arr.size();
+
+        int ptra = 0, ptrb = size - 1;
+
+        while(ptra <= ptrb) {
+
+            if(arr[ptra] < arr[ptra+1]) {
+                ++ptra;
+            }
+
+            if(arr[ptrb-1] > arr[ptrb]) {
+                --ptrb;
+            }
+
+            if(ptra == ptrb) {
+                return 1;
+            }
+            else if(arr[ptra] > arr[ptra+1] || arr[ptrb-1] > arr[ptrb]) {
+                return 0;
+            }
+
+
+        }
+
+        return 1;
+    }
+};
