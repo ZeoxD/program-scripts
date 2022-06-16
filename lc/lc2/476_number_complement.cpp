a67cd49d5f5c4c208936b1b36c0cdb16329696f5
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-06-16 19:22:18 +0530

    Jun 16 2022

---
+++ lc/lc2/476_number_complement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findComplement(int n) {
+        vector<int> bin;
+        
+        if(n == 0) {
+            return 1;
+        }
+
+        while(n>0) {
+            bin.push_back(n%2);
+            n = n/2;
+        }
+        int cmp = 0;
+        for(int i = 0; i < bin.size(); i++) {
+            if(bin[i] == 0) {
+                cmp += pow(2, i);
+            }
+        }
+
+        return cmp;
+    }
+};
