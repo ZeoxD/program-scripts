92148297039d8bfc4d2fe9312097e11ee35b5baf
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-06-18 23:52:49 +0530

    Jun 18 2022

---
+++ lc/lc2/744_smallest_letter.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int size = letters.size();
+        
+        for(int i = 0; i < size; i++) {
+            if(letters[i] > target) {
+                return letters[i];
+            }
+        }
+        
+        return letters[0];
+    }
+};
