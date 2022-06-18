c1d1559c8d54de892fa60714929d4dc7553b511e
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-06-19 02:00:55 +0530

    Jun 19 2022

--- lc/lc2/628_maximum_product.cpp
+++ lc/lc2/628_maximum_product.cpp
@@ -10,13 +10,18 @@ public:
         sort(nums.begin(), nums.end());
 
         int pro = 1;
+        int npro = 1;
         int size = nums.size();
 
         for(int i = size-1; i > size-1-3; i--) {
             pro = pro * nums[i];
         }
+        for(int i = 0; i < 2; i++) {
+            npro = npro * nums[i];
+        }
+        npro = npro * nums[size-1];
 
-        return pro;
+        return max(pro, npro);
     }
 
 };
