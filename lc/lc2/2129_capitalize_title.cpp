d8c9c9d6be786f663eddc375f20e428f212f5cb3
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-06-23 19:01:40 +0530

    Jun 23 2022

--- lc/lc2/2129_capitalize_title.cpp
+++ lc/lc2/2129_capitalize_title.cpp
@@ -5,7 +5,51 @@ using namespace std;
 
 class Solution {
 public:
-    string capitalizeTitle(string title) {
+    string capitalizeTitle(string stitle) {
+        string title = "";
+        title += ' ';
+        title += stitle;
+        title += ' ';
         
+        int size = title.length();
+        int i = 0; 
+        int j = i+1;
+
+        while(i <= j && j < size) {
+            if(title[j] != ' ') {
+                ++j;
+            }
+            else if(title[i] == ' ' && title[j] == ' ' && j-i-1 >=3) {
+                ++i;
+                if(title[i] >= 97 && title[i] <= 122) {
+                    title[i] = title[i] - 32;
+                }
+                ++i;
+                while(i<=j) {
+                    if(title[i] >= 65 && title[i] <= 90 ) {
+                        title[i] = title[i] + 32;
+                    }
+                    ++i;
+                }
+                --i;
+                ++j;
+            }
+            else if(title[i] == ' ' && title[i] == ' ' && j-i-1 <=2) {
+                ++i;
+                while(i<=j) {
+                    if(title[i] >= 65 && title[i] <= 90) {
+                        title[i] = title[i] + 32;
+
+                    }
+                    ++i;
+                }
+                --i;
+                ++j;
+            }
+        }
+        
+
+        return title.substr(1,size-2);
+
     }
 };
