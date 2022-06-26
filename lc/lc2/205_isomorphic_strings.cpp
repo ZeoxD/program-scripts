efa1b9ce841156213a6f4d175b3db5f625a0ff76
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-06-26 16:35:56 +0530

    Jun 26 2022

--- lc/lc2/205_isomorphic_strings.cpp
+++ lc/lc2/205_isomorphic_strings.cpp
@@ -7,19 +7,21 @@ using namespace std;
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, bool> uma;
-        unordered_map<char, bool> umb;
+        unordered_map<char, int> sc;
+        unordered_map<char, int> tc;
+        unordered_map<char, char> umap;
 
         int size = s.size();
 
         for(int i = 0; i < size; i++) {
 
-            if(uma[s[i]]) {
+            if( (sc[s[i]] > 0 || tc[t[i]] > 0) && umap[s[i]] != t[i] ) {
                 return false;
             }
             
-            uma[s[i]] = t[i];
-            umb[t[i]] = s[i];
+            sc[s[i]]++;
+            tc[t[i]]++;
+            umap[s[i]] = t[i];
 
         }
 
