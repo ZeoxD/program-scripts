7141aa27d9bcd526bd4f9a555836f44311532118
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-07-05 12:23:05 +0530

    Jul 5 2022

---
+++ lc/lc2/884_uncommon_words.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+
+        for(int i = 0; i < len1; i++) {
+            
+        }
+    }
+};
