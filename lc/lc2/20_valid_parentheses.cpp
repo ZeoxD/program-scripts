3cfd3eae4d2ce08cc0a5d245db99f33c666abd47
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-07-14 18:58:22 +0530

    Jul 14 2022

--- lc/lc2/20_valid_parentheses.cpp
+++ lc/lc2/20_valid_parentheses.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 using namespace std;
 
 class Solution {
 public:
     bool isValid(string s) {
-        
+        vector<int> vtr;
+
+        for(const auto &c: s) {
+            if(!vtr.empty() && vtr.back() == '(' && c == ')') {
+                vtr.pop_back();
+            }
+            else if(!vtr.empty() && vtr.back() == '{' && c == '}') {
+                vtr.pop_back();
+            }
+            else if(!vtr.empty() && vtr.back() == '[' && c == ']') {
+                vtr.pop_back();
+            }
+
+
+            vtr.push_back(c);
+        }
+
+        if(vtr.empty()) {
+            return true;
+        }
+
+        return false;
     }
 };
