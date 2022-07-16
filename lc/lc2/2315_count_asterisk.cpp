1b11bd25a41c4c728bc3d2ce02d19449ca6f7db1
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-07-16 21:06:23 +0530

    Jul 16 2022

---
+++ lc/lc2/2315_count_asterisk.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Solution {
+public:
+    int countAsterisks(string s) {
+        int size = s.length();
+        bool pipe = false;
+        int aster = 0;
+        for(int i = 0; i < size; i++) {
+            if(s[i] == '*' || s[i] == '|') {
+                if(s[i] == '|') pipe = !pipe;
+
+                if(!pipe && s[i] == '*')  {
+                    ++aster;
+                }
+
+                
+            }
+        }
+
+
+    }
+};
