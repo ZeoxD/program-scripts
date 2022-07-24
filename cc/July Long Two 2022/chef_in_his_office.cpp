bd3199aaf4d753856bd191649123e1767e12630b
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-07-24 13:16:12 +0530

    Jul 24 2022

---
+++ cc/July Long Two 2022/chef_in_his_office.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#define nl "\n"
+using namespace std;
+
+int main() {
+	// your code goes here
+	int tt;
+	cin>>tt;
+	while(tt--) {
+	    int x, y;
+	    cin>>x>>y;
+	    
+	    cout<<((x*4)+y)<<nl;
+	}
+	
+	return 0;
+}
