bb3f428cd777ae903e532ea5698f677d585137c4
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-08-03 02:05:19 +0530

    Aug 3 2022

--- lc/lc3/1351_negative_number.cpp
+++ lc/lc3/1351_negative_number.cpp
@@ -4,8 +4,28 @@
 using namespace std;
 
 class Solution {
+    int bs(vector<int> v) {
+        int size = v.size();
+        int l=0, mid;
+        while(l<=size) {
+            mid = (l + (size - l))/2;
+            if(mid < 0) {
+                size = mid-1;
+            }
+            else if(mid > 0) {
+                l = mid+1;
+            }
+        }
+        cout<<mid;
+        return mid;
+    }
 public:
     int countNegatives(vector<vector<int>>& grid) {
-        
+        int nve = 0;
+        for(auto g: grid) {
+            nve += bs(g);
+        }
+
+        return nve;
     }
 };
