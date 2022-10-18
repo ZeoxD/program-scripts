cc6f0124e92132fddd582c859caab868805c6787
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-10-19 01:36:01 +0530

    Oct 19 2022

---
+++ cc/Practice/cupcakes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t; cin>>t; 
+	while(t--) {
+	    long n; cin>>n;
+	    long x = n/2+1;
+	    cout<<x<<endl;
+	    
+	}
+	return 0;
+}
