94f716f20aee948028a92fc1bc7d4e1ca0d284ae
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-10-21 23:57:02 +0530

    Oct 21 2022

--- cc/Practice/cricket_rank.cpp
+++ cc/Practice/cricket_rank.cpp
@@ -9,14 +9,13 @@ int main() {
 	    cin>>r>>w>>c;
 	    int R, W, C;
 	    cin>>R>>W>>C;
-	    int a, b;
+	    int a = 0, b = 0;
 	    if(r>R) ++a;
 	    else ++b;
 	    if(w>W) ++a;
 	    else ++b;
 	    if(c>C) ++a;
 	    else ++b;
-	    cout<<a<<b<<endl;
 	    if(a>b) cout<<"A"<<endl;
 	    else cout<<"B"<<endl;
 	}
