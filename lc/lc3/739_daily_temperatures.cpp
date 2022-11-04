71bdfd95bda4fedd941668f9413322c75da1e4f4
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2022-11-04 22:12:35 +0530

    Nov 4 2022

--- lc/lc3/739_daily_temperatures.cpp
+++ lc/lc3/739_daily_temperatures.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 #include<vector>
+#include<stack>
 using namespace std;
 
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        vector<int> vtr(temp.size(), 0);
+        stack<pair<int, int>> st;
+        for(int i = 0; i < temp.size(); i++) {
+            while(!st.empty() and temp[i] > st.top().second) {
+                
+            }
+            st.push(make_pair(i, temp[i]));
+        }
+        return vtr;
     }
 };
