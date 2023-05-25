012d42922880d875d73f2248fc0a8fc921e3f3f4
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2023-05-25 16:47:21 +0530

    SQL statement using Subqueries and Joins

---
+++ lc/db/550_game_play_analysis_iv.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT ROUND(COUNT(DISTINCT b.player_id)/ COUNT(DISTINCT a.player_id),2) as "fraction" FROM
+(SELECT player_id, MIN(event_date) AS "event_date" 
+FROM Activity
+GROUP BY player_id) AS a LEFT JOIN Activity AS b 
+ON a.player_id = b.player_id 
+AND a.event_date+1 = b.event_date
