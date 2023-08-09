54e396e88ce45252cb6738b8d5235bcd385bac4a
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2023-08-09 20:56:55 +0530

    swap nodes from left and right by k value

---
+++ lc/lc4/1721_swapping_linked_list.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *iter = head, *left = head, *right = head;
+        
+        while(iter) {
+            if(--k >= 0) {
+                left = iter;
+            }
+            else {
+                right = right->next;   
+            }
+            iter = iter->next;
+        }c
+        
+        int temp = left->val;
+        left->val = right->val;
+        right->val = temp;
+        
+        return head;
+    }
+};
