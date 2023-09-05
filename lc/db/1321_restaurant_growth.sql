969edf7ac37be81dcef5b314042b600b8938c442
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2023-09-06 01:11:18 +0530

    SQL query updated without HAVING clause

--- lc/db/1321_restaurant_growth.sql
+++ lc/db/1321_restaurant_growth.sql
@@ -1,10 +1,11 @@
 # Write your MySQL query statement below
 
-SELECT a.visited_on, SUM(b.day_sum) AS amount,
-ROUND(SUM(b.day_sum)/7, 2) AS average_amount
+SELECT a.visited_on, ROUND(SUM(b.day_sum), 2) AS 'amount', 
+ROUND(SUM(b.day_sum)/ 7, 2) AS 'average_amount'
 FROM
     (SELECT visited_on, SUM(amount) AS day_sum FROM Customer GROUP BY visited_on) AS a,
     (SELECT visited_on, SUM(amount) AS day_sum FROM Customer GROUP BY visited_on) AS b
 WHERE DATEDIFF(a.visited_on, b.visited_on) BETWEEN 0 AND 6
+AND a.visited_on >= (SELECT min(visited_on) FROM Customer) + 6
 GROUP BY a.visited_on
-HAVING COUNT(b.visited_on) = 7
+ORDER BY a.visited_on
