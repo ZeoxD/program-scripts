1fd891d1cb5b53a336005b05144f10ef3c77415e
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2023-11-03 04:29:56 +0530

    Remove duplicate letters using Greedy method modified

--- lc/lc4/316_remove_duplicate_letters.cpp
+++ lc/lc4/316_remove_duplicate_letters.cpp
@@ -12,25 +12,22 @@ public:
         for(int i = 0; i < s.length(); i++) {
             if(vis.find(s[i]) != vis.end()) {
                 continue;
-            }      
-            cout<<occur[s[i]]<<" > "<<i<<endl;
-            while(!st.empty() && (st.top() > s[i]) && (occur[s[i]] > i)) {
+            } 
+            while(!st.empty() && (st.top() > s[i]) && (occur[st.top()] > i)) {
                 vis.erase(st.top());
                 st.pop();
             }
-
             st.push(s[i]);
             vis.insert(s[i]);
         }
-        
+
         string str = "";
         while(!st.empty()) {
             str.push_back(st.top());
             st.pop();
         }
-
+        
         reverse(str.begin(), str.end());
-
         return str;
     }
 };
