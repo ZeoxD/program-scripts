85a73abeb7881e82dc846f20d6d937e80b8ccacb
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2023-12-31 02:43:48 +0530

    Modified code for Car fleet size using stack method

--- lc/lc4/853_car_fleet.cpp
+++ lc/lc4/853_car_fleet.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        vector<pair<int, int>> vtr;
-        stack<int> st;
+        vector<pair<long, long>> vtr;
+        stack<long> st;
         int size = position.size();
         for(int i = 0; i < size; i++) {
             vtr.push_back({position[i], speed[i]});
         }
 
         sort(vtr.rbegin(), vtr.rend());
-        int last;
+        long last;
 
         for(int i = 0; i < size; i++) {
-            st.push((target - vtr[i].first)/vtr[i].second);
-            if(st.size()>=2 ) {
-                int first = st.top();
+            st.push( (target - vtr[i].first)*1000000 / vtr[i].second);
+            
+            if(st.size() >= 2 && st.top() <= last) {
                 st.pop();
-                int second = st.top();
-                st.push(first);
-                if(first <= second) {
-                    st.pop();
-                }
             }
+            
+            last = st.top();
+
         }
 
         int res = st.size();
