0da04f2ee16059dfa52044786eeeeaaa4eaff3df
Divyanshu Tripathi <zeoxd.tripathi@gmail.com>
2024-01-11 23:44:10 +0530

    largest palindromic substring using Greedy method

--- lc/lc3/5_longest_palindromic_substring.cpp
+++ lc/lc3/5_longest_palindromic_substring.cpp
@@ -10,26 +10,27 @@ public:
 
         for(int i = 0; i < s.length(); i++) {
             int l = i, r = i;
-            while(l >= 0 and r < s.length() and s[i] == s[r]) {
+            while(l >= 0 && r < s.length() && s[l] == s[r]) {
                 if((r-l+1) > strmax) {
-                    str = s.substr(l, r+1);
+                    str = s.substr(l, r-l+1);
                     strmax = r-l+1;
                 }
-                l = l - 1;
-                r = r + 1;
+                l--;
+                r++;
             }
 
-            int l = i, r = i + 1;
-            while(l >= 0 and r < s.length()-1 and s[i] == s[r]) {
+            l = i, r = i + 1;
+            while(l >= 0 && r < s.length() && s[l] == s[r]) {
                 if((r-l+1) > strmax) {
-                    str = s.substr(l, r+1);
+                    str = s.substr(l, r-l+1);
                     strmax = r-l+1;
                 }
-                l = l-1;
-                r = r+1;
+                l--;
+                r++;
             }
         }
 
         return str;
     }
 };
+
